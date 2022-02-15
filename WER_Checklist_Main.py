6b3bb880b90737bf29ba34172588c4599acfde20
Suilarso <suilarso@gmail.com>
2022-02-14 22:32:53 -0600

    add codes to implement check button for number of parts fields

--- WER_Checklist_Main.py
+++ WER_Checklist_Main.py
@@ -29,9 +29,9 @@ RECEIVED_BY_ENTRY_COL = 5
 PIECES_LABEL_ROW = 4
 PIECES_LABEL_COL = 1
 PIECES_ENTRY_ROW = 4
-PIECES_LABEL_COL = 2
+PIECES_ENTRY_COL = 2
 OF_LABEL_ROW = 4
-OF_LABEL_COL = 1
+OF_LABEL_COL = 4
 OF_ENTRY_ROW = 4
 OF_ENTRY_COL = 5
 PICTURE_CHECK_BUTTON_ROW = 4
@@ -42,12 +42,45 @@ PRODUCTS_TYPE_LABEL_COL = 1
 PRODUCTS_TYPE_LIST_BOX_ROW = 6
 PRODUCTS_TYPE_LIST_BOX_COL = 2
 
+FITTINGS_CHECK_BUTTON_ROW = 6
+FITTINGS_CHECK_BUTTON_COL = 5
+HOSES_CHECK_BUTTON_ROW = 7
+HOSES_CHECK_BUTTON_COL = 5
+SENSORS_CHECK_BUTTON_ROW = 8
+SENSORS_CHECK_BUTTON_COL = 5
+BRACKETS_CHECK_BUTTON_ROW = 9
+BRACKETS_CHECK_BUTTON_COL = 5
+MOUNTS_CHECK_BUTTON_ROW = 10
+MOUNTS_CHECK_BUTTON_COL = 5
+RAD_CAP_CHECK_BUTTON_ROW = 11
+RAD_CAP_CHECK_BUTTON_COL = 5
+SHROUD_CHECK_BUTTON_ROW = 12
+SHROUD_CHECK_BUTTON_COL = 5
+FAN_CHECK_BUTTON_ROW = 13
+FAN_CHECK_BUTTON_COL = 5
+CHAIN_CHECK_BUTTON_ROW = 14
+CHAIN_CHECK_BUTTON_COL = 5
+STRAPS_CHECK_BUTTON_ROW = 15
+STRAPS_CHECK_BUTTON_COL = 5
 
 #SJ0130222 - Ideally the products type list should be stored in a database; the setback of reading from a List
 #SJ0130222 - is that there is a need to write a code to allow user to do data entry into the database.
 productsType = ['Radiator', 'CAC', 'Condenser', 'Fuel Tank', 'Evaporator', 'Heater Core', 'Radiator Core',
 'DPF', 'DPF-DOC Combo', 'EGR', 'Coolant Pipe', 'Oil Cooler', 'AC Hose', 'Cooling Module', 'Other']
 
+FITTINGS = 0
+HOSES = 1
+SENSORS = 2
+BRACKETS = 3
+MOUNTS = 4
+RAD_CAP = 5
+SHROUD = 6
+FAN = 7
+CHAIN = 8
+STRAPS = 9
+TOTAL_NUMBER_OF_PARTS = 10
+
+numberOfParts = [0] * TOTAL_NUMBER_OF_PARTS
 
 class WER_Main:
     def __init__(self, master):
@@ -76,7 +109,7 @@ class WER_Main:
         #SJ6120222 - Input field for pieces
         self.piecesLabel = Label(master, text='Piece(s): ').grid(row=PIECES_LABEL_ROW, column=PIECES_LABEL_COL)
         self.piecesEntry = Entry(master)
-        self.piecesEntry.grid(row=PIECES_ENTRY_ROW, column=PIECES_LABEL_COL)
+        self.piecesEntry.grid(row=PIECES_ENTRY_ROW, column=PIECES_ENTRY_COL)
 
         #SJ6120222 - Input field for of
         self.ofLabel = Label(master, text='of: ').grid(row=OF_LABEL_ROW, column=OF_LABEL_COL)
@@ -96,6 +129,48 @@ class WER_Main:
         # self.productsTypelistbox.pack()
         self.productsTypelistbox.grid(row=PRODUCTS_TYPE_LIST_BOX_ROW, column=PRODUCTS_TYPE_LIST_BOX_COL)
 
+        #SJ1140222 - Check button fields for number of parts
+        numberOfPartsFrame = Frame()
+        numberOfParts[FITTINGS] = IntVar()
+        self.fittingsCheckButton = Checkbutton(numberOfPartsFrame, text='Fittings ', var=numberOfParts[FITTINGS])
+        self.fittingsCheckButton.grid(row=FITTINGS_CHECK_BUTTON_ROW, column=FITTINGS_CHECK_BUTTON_COL)
+
+        numberOfParts[HOSES] = IntVar()
+        self.hosesCheckButton = Checkbutton(numberOfPartsFrame, text='Hoses ', var=numberOfParts[HOSES])
+        self.hosesCheckButton.grid(row=HOSES_CHECK_BUTTON_ROW, column=HOSES_CHECK_BUTTON_COL)
+
+        numberOfParts[SENSORS] = IntVar()
+        self.sensorsCheckButton = Checkbutton(numberOfPartsFrame, text='Sensors ', var=numberOfParts[SENSORS])
+        self.sensorsCheckButton.grid(row=SENSORS_CHECK_BUTTON_ROW, column=SENSORS_CHECK_BUTTON_COL)
+
+        numberOfParts[BRACKETS] = IntVar()
+        self.bracketsCheckButton = Checkbutton(numberOfPartsFrame, text='Brackets ', var=numberOfParts[BRACKETS])
+        self.bracketsCheckButton.grid(row=BRACKETS_CHECK_BUTTON_ROW, column=BRACKETS_CHECK_BUTTON_COL)
+
+        numberOfParts[MOUNTS] = IntVar()
+        self.mountCheckButton = Checkbutton(numberOfPartsFrame, text='Mount ', var=numberOfParts[MOUNTS])
+        self.mountCheckButton.grid(row=MOUNTS_CHECK_BUTTON_ROW, column=MOUNTS_CHECK_BUTTON_COL)
+
+        numberOfParts[RAD_CAP] = IntVar()
+        self.radCapCheckButton = Checkbutton(numberOfPartsFrame, text='Rad Cap ', var=numberOfParts[RAD_CAP])
+        self.radCapCheckButton.grid(row=RAD_CAP_CHECK_BUTTON_ROW, column=RAD_CAP_CHECK_BUTTON_COL)
+
+        numberOfParts[SHROUD] = IntVar()
+        self.shroudCheckButton = Checkbutton(numberOfPartsFrame, text='Shroud ', var=numberOfParts[SHROUD])
+        self.shroudCheckButton.grid(row=SHROUD_CHECK_BUTTON_ROW, column=SHROUD_CHECK_BUTTON_COL)
+
+        numberOfParts[FAN] = IntVar()
+        self.fanCheckButton = Checkbutton(numberOfPartsFrame, text='Fan ', var=numberOfParts[FAN])
+        self.fanCheckButton.grid(row=FAN_CHECK_BUTTON_ROW, column=FAN_CHECK_BUTTON_COL)
+
+        numberOfParts[CHAIN] = IntVar()
+        self.chainCheckButton = Checkbutton(numberOfPartsFrame, text='Chain ', var=numberOfParts[CHAIN])
+        self.chainCheckButton.grid(row=CHAIN_CHECK_BUTTON_ROW, column=CHAIN_CHECK_BUTTON_COL)
+
+        numberOfParts[STRAPS] = IntVar()
+        self.strapsCheckButton = Checkbutton(numberOfPartsFrame, text='Straps ', var=numberOfParts[STRAPS])
+        self.strapsCheckButton.grid(row=STRAPS_CHECK_BUTTON_ROW, column=STRAPS_CHECK_BUTTON_COL)
+        numberOfPartsFrame.grid(row=FITTINGS_CHECK_BUTTON_ROW, column=FITTINGS_CHECK_BUTTON_COL)
 
         #self.button = Button(text='Click me', command=self.callback)
         #self.label.grid(row=0, column=0)
