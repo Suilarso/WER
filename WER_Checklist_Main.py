5dcfdd1c48a72360f8ce5537095f6942fc4f17f5
Suilarso <suilarso@gmail.com>
2022-02-21 11:20:24 -0600

    Make data entry codes into a function and make row and col dependant on previous row and col

--- WER_Checklist_Main.py
+++ WER_Checklist_Main.py
@@ -7,81 +7,66 @@ from tkcalendar import DateEntry
 
 #SJ0130222 - Global variables declaration
 
-#SJ6120222 - Row and Column position of entry fields
+#SJ1210222 - SJ6120222 - Row and Column position of entry fields
 CUSTOMER_LABEL_ROW = 1
 CUSTOMER_LABEL_COL = 1
-CUSTOMER_ENTRY_ROW = 1
-CUSTOMER_ENTRY_COL = 2
-WORK_ORDER_LABEL_ROW = 1
-WORK_ORDER_LABEL_COL = 4
-WORK_ORDER_ENTRY_ROW = 1
-WORK_ORDER_ENTRY_COL = 5
-
-DATE_RECEIVED_LABEL_ROW = 3
-DATE_RECEIVED_LABEL_COL = 1
-DATE_RECEIVED_ENTRY_ROW = 3
-DATE_RECEIVED_ENTRY_COL = 2
-RECEIVED_BY_LABEL_ROW = 3
-RECEIVED_BY_LABEL_COL = 4
-RECEIVED_BY_ENTRY_ROW = 3
-RECEIVED_BY_ENTRY_COL = 5
-
-PIECES_LABEL_ROW = 4
-PIECES_LABEL_COL = 1
-PIECES_ENTRY_ROW = 4
-PIECES_ENTRY_COL = 2
-OF_LABEL_ROW = 4
-OF_LABEL_COL = 4
-OF_ENTRY_ROW = 4
-OF_ENTRY_COL = 5
-PICTURE_CHECK_BUTTON_ROW = 5
-PICTURE_CHECK_BUTTON_COL = 1
-PHOTOES_CHECK_BUTTON_ROW = 5
-PHOTOES_CHECK_BUTTON_COL = 4
-
-PRODUCTS_TYPE_LABEL_ROW = 6
-PRODUCTS_TYPE_LABEL_COL = 1
-PRODUCTS_TYPE_LIST_BOX_ROW = 6
-PRODUCTS_TYPE_LIST_BOX_COL = 2
-
-NUMBER_OF_PARTS_LIST_BOX_ROW = 6
-NUMBER_OF_PARTS_LIST_BOX_COL = 5
-
-FITTINGS_CHECK_BUTTON_ROW = 6
-FITTINGS_CHECK_BUTTON_COL = 5
-HOSES_CHECK_BUTTON_ROW = 7
-HOSES_CHECK_BUTTON_COL = 5
-SENSORS_CHECK_BUTTON_ROW = 8
-SENSORS_CHECK_BUTTON_COL = 5
-BRACKETS_CHECK_BUTTON_ROW = 9
-BRACKETS_CHECK_BUTTON_COL = 5
-MOUNTS_CHECK_BUTTON_ROW = 10
-MOUNTS_CHECK_BUTTON_COL = 5
-RAD_CAP_CHECK_BUTTON_ROW = 11
-RAD_CAP_CHECK_BUTTON_COL = 5
-SHROUD_CHECK_BUTTON_ROW = 12
-SHROUD_CHECK_BUTTON_COL = 5
-FAN_CHECK_BUTTON_ROW = 13
-FAN_CHECK_BUTTON_COL = 5
-CHAIN_CHECK_BUTTON_ROW = 14
-CHAIN_CHECK_BUTTON_COL = 5
-STRAPS_CHECK_BUTTON_ROW = 15
-STRAPS_CHECK_BUTTON_COL = 5
-
-PARTS_IN_BLUE_BIN_ROW = 17
+CUSTOMER_ENTRY_ROW = CUSTOMER_LABEL_ROW          #row 1
+CUSTOMER_ENTRY_COL = CUSTOMER_LABEL_COL + 1      #col 2
+WORK_ORDER_LABEL_ROW = CUSTOMER_LABEL_ROW        #row 1
+WORK_ORDER_LABEL_COL = CUSTOMER_ENTRY_COL + 2    #col 4
+WORK_ORDER_ENTRY_ROW = CUSTOMER_LABEL_ROW        #row 1
+WORK_ORDER_ENTRY_COL = WORK_ORDER_LABEL_COL + 1  #col 5
+#SJ1210222 - Row 2 not used
+DATE_RECEIVED_LABEL_ROW = WORK_ORDER_ENTRY_ROW + 2    #row 3
+DATE_RECEIVED_LABEL_COL = 1                           #col 1
+DATE_RECEIVED_ENTRY_ROW = DATE_RECEIVED_LABEL_ROW     #row 3
+DATE_RECEIVED_ENTRY_COL = DATE_RECEIVED_LABEL_COL + 1 #col 2
+RECEIVED_BY_LABEL_ROW = DATE_RECEIVED_LABEL_ROW       #row 3
+RECEIVED_BY_LABEL_COL = DATE_RECEIVED_ENTRY_COL + 2   #col 4
+RECEIVED_BY_ENTRY_ROW = DATE_RECEIVED_LABEL_ROW       #row 3
+RECEIVED_BY_ENTRY_COL = RECEIVED_BY_LABEL_COL + 1     #col 5
+#SJ1210222 - row 4 not used
+PIECES_LABEL_ROW = RECEIVED_BY_ENTRY_ROW + 2 #row 5
+PIECES_LABEL_COL = 1                         #col 1
+PIECES_ENTRY_ROW = PIECES_LABEL_ROW          #row 5
+PIECES_ENTRY_COL = PIECES_LABEL_COL + 1      #col 2
+OF_LABEL_ROW = PIECES_LABEL_ROW              #row 5
+OF_LABEL_COL = PIECES_ENTRY_COL + 2          #col 4
+OF_ENTRY_ROW = PIECES_LABEL_ROW              #row 5
+OF_ENTRY_COL = OF_LABEL_COL + 1              #col 5
+#SJ1210222 - row 6 not used
+PICTURE_CHECK_BUTTON_ROW = OF_ENTRY_ROW + 2             #row 7
+PICTURE_CHECK_BUTTON_COL = 1                            #col 1
+PHOTOES_CHECK_BUTTON_ROW = PICTURE_CHECK_BUTTON_ROW     #row 7
+PHOTOES_CHECK_BUTTON_COL = PICTURE_CHECK_BUTTON_COL + 3 #col 4
+#SJ1210222 - row 8 not used
+#SJ1210222 - Products type and num of parts list box occupy row 9 to row 18
+PRODUCTS_TYPE_LABEL_ROW = PHOTOES_CHECK_BUTTON_ROW + 2       #row 9
+PRODUCTS_TYPE_LABEL_COL = 1                                  #col 1
+PRODUCTS_TYPE_LIST_BOX_ROW = PRODUCTS_TYPE_LABEL_ROW         #row 9
+PRODUCTS_TYPE_LIST_BOX_COL = PRODUCTS_TYPE_LABEL_COL + 1     #col 2
+NUMBER_OF_PARTS_LABEL_ROW = PRODUCTS_TYPE_LABEL_ROW          #row 9 - NEW
+NUMBER_OF_PARTS_LABEL_COL = PRODUCTS_TYPE_LIST_BOX_COL + 2   #col 4 - NEW
+NUMBER_OF_PARTS_LIST_BOX_ROW = PRODUCTS_TYPE_LABEL_ROW       #row 9
+NUMBER_OF_PARTS_LIST_BOX_COL = NUMBER_OF_PARTS_LABEL_COL + 1 #col 5
+#SJ1210222 - row 19 not used
+PARTS_IN_BLUE_BIN_ROW = NUMBER_OF_PARTS_LIST_BOX_ROW + 11 #row 20
 PARTS_IN_BLUE_BIN_COL = 1
-NOTE_LABEL_ROW = 25
-NOTE_LABEL_COL = 2
-NOTE_ENTRY_ROW = 25
-NOTE_ENTRY_COL = 3
+#SJ1210222 - row 21 not used
+NOTE_LABEL_ROW = PARTS_IN_BLUE_BIN_ROW + 2 #row 22
+NOTE_LABEL_COL = 2                         #col 2
+NOTE_ENTRY_ROW = NOTE_LABEL_ROW            #row 22
+NOTE_ENTRY_COL = NOTE_LABEL_COL + 1        #col 3
+#SJ1210222 - row 23 not used
+CANCEL_BUTTON_ROW = NOTE_ENTRY_ROW + 2  #row 24
+CANCEL_BUTTON_COL = 1                   #col 1
+SAVE_BUTTON_ROW = CANCEL_BUTTON_ROW     #row 24
+SAVE_BUTTON_COL = CANCEL_BUTTON_COL + 4 #col 5
 
+#SJ1210222 - Output pad is used for debug purpose only it is not part of the date entry
 OUTPUT_PAD_ROW = 28
 OUTPUT_PAD_COL = 2
 
-CANCEL_BUTTON_ROW = 28
-CANCEL_BUTTON_COL = 1
-SAVE_BUTTON_ROW = 28
-SAVE_BUTTON_COL = 6
 
 #SJ0130222 - Ideally the products type list should be stored in a database; the setback of reading from a database
 #SJ0130222 - is that there is a need to write a code to allow user to do data entry into the database.
@@ -89,9 +74,6 @@ productsType = ['Radiator', 'CAC', 'Condenser', 'Fuel Tank', 'Evaporator', 'Heat
 'DPF', 'DPF-DOC Combo', 'EGR', 'Coolant Pipe', 'Oil Cooler', 'AC Hose', 'Cooling Module', 'Other']
 
 numberOfParts = ['Fitting', 'Hoses', 'Sensors', 'Brackets', 'Mounts', 'Rad Cap', 'Shroud', 'FAN', 'Chain', 'Straps']
-#TOTAL_NUMBER_OF_PARTS = 10
-
-#numberOfParts = [0] * TOTAL_NUMBER_OF_PARTS
 
 customerName = ''
 workOrder = ''
@@ -110,7 +92,9 @@ outputPad = ''
 class WER_Main:
     def __init__(self, master):
         master.title('West End Radiators')
+        self.setupDataEntryScreen(master)
 
+    def setupDataEntryScreen(self, master):
         global customerName
         global workOrder
         global dateReceived
@@ -202,8 +186,6 @@ class WER_Main:
     def cancelCallback(self, master):
         #global outputPad
         global customerName
-        #self.custo = customerName.get()
-        #outputPad.insert(0, self.custo)
 
     def saveCallback(self, master):
         global outputPad
@@ -216,23 +198,7 @@ class WER_Main:
         print('product type list: ', ''.join(list(map(str, self.numberOfPartsList))))
         #self.label.configure(text='Button clicked')
 
-        #a = [4, 3, 2, 1, 10, 14, -14]
-        #print(list(map(str, a)))
-
-
 
 root = Tk()
-
 app = WER_Main(root)
-
 mainloop()
-
-"""
-label = Label(text='Not clicked')
-button = Button(text='Click me', command=callback)
-label.grid(row=0, column=0)
-button.grid(row=1, column=0)
-
-Button(text=alphabet[i], command = lambda x=i: callback(x))
-
-"""
